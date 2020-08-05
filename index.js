681c89aacc052cf387efe3fe8e2cd01b060c8636
Daniel Ogbuti <danielogbuti@gmail.com>
2020-08-05 02:14:32 +0100

    changes

---
+++ index.js
@@ -0,0 +1,65 @@
+var buttons = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        // This returns the element that triggered an event
+        var buttonImageHTML = this.innerHTML;
+        checkAndPlaySound(buttonImageHTML);
+        buttonAnimation(buttonImageHTML);
+
+    });
+
+}
+
+document.addEventListener("keydown", function (event) {
+    checkAndPlaySound(event.key);
+    buttonAnimation(event.key);
+});
+
+function checkAndPlaySound(character) {
+    switch (character) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        default:
+            console.log(buttonImageHTML);
+            break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(() => {
+        activeButton.classList.remove("pressed"
+        )
+    }, 100);
+}
+
